ddf6a6e1366dedda685e99df82d2e13f7887fb63
dardanmat <dardan.berisha@stud.unifi.it>
2023-06-21 09:12:09 +0200

    First Commit

--- ActivityLog.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by Dardan Matias Berisha on 19/06/23.
-//
-
-#include "ActivityLog.h"
-
--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <chrono>
-#include "Activity.h"
 int main() {
 
-    auto a = new Activity("Campeggio", "diocane", "merda");
+    std::cout << "Hello world" << std::endl;
 
-
-    std::cout << "Hello world" << a->getDescription() << std::endl;
     return 0;
 }
